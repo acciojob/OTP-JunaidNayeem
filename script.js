338f207ba54caa731ab5941db98cd93a495bb244
Junaid Nayeem <78254107+JunaidNayeem@users.noreply.github.com>
2024-03-05 00:00:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+const codeInputs = document.querySelectorAll(".code");
+
+codeInputs.forEach((input, index) => {
+  input.addEventListener("keyup", (event) => {
+    if (event.key === "Backspace" && input.value === "") {
+      if (index > 0) {
+        codeInputs[index - 1].focus();
+        codeInputs[index - 1].value = "";
+      }
+    } else if (input.value.length === 1) {
+      if (index < codeInputs.length - 1) {
+        codeInputs[index + 1].focus();
+      }
+    }
+  });
+});
